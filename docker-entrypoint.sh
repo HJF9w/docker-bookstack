0beda9d5611a56e2918b1eb8cc79afcb83751cfc
xenon <xenon@localhost.local>
2022-07-12 13:57:25 +0200

    changes to make the image work on arm

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,17 +14,14 @@ if [ ! -f ".env" ]; then
       APP_ENV=production
       APP_DEBUG=${APP_DEBUG:-false}
       APP_KEY=${APP_KEY:-SomeRandomStringWith32Characters}
-
       # The below url has to be set if using social auth options
       # or if you are not using BookStack at the root path of your domain.
       APP_URL=${APP_URL:-null}
-
       # Database details
-      DB_HOST=${DB_HOST:-localhost}
+      DB_HOST=${DB_HOST:-db}
       DB_DATABASE=${DB_DATABASE:-bookstack}
       DB_USERNAME=${DB_USERNAME:-bookstack}
-      DB_PASSWORD=${DB_PASSWORD:-password}
-
+      DB_PASSWORD=${DB_PASSWORD:-secret}
       # Cache and session
       CACHE_DRIVER=file
       SESSION_DRIVER=file
@@ -32,13 +29,11 @@ if [ ! -f ".env" ]; then
       #CACHE_DRIVER=memcached
       #SESSION_DRIVER=memcached
       QUEUE_DRIVER=sync
-
       # Memcached settings
       # If using a UNIX socket path for the host, set the port to 0
       # This follows the following format: HOST:PORT:WEIGHT
       # For multiple servers separate with a comma
       MEMCACHED_SERVERS=127.0.0.1:11211:100
-
       # Storage
       STORAGE_TYPE=${STORAGE_TYPE:-local}
       # Amazon S3 Config
@@ -49,19 +44,15 @@ if [ ! -f ".env" ]; then
       # Storage URL
       # Used to prefix image urls for when using custom domains/cdns
       STORAGE_URL=${STORAGE_URL:-false}
-
       # General auth
       AUTH_METHOD=${AUTH_METHOD:-standard}
-
       # Social Authentication information. Defaults as off.
       GITHUB_APP_ID=${GITHUB_APP_ID:-false}
       GITHUB_APP_SECRET=${GITHUB_APP_SECRET:-false}
       GOOGLE_APP_ID=${GOOGLE_APP_ID:-false}
       GOOGLE_APP_SECRET=${GOOGLE_APP_SECRET:-false}
-
       # External services such as Gravatar
       DISABLE_EXTERNAL_SERVICES=${DISABLE_EXTERNAL_SERVICES:-false}
-
       # LDAP Settings
       LDAP_SERVER=${LDAP_SERVER:-false}
       LDAP_BASE_DN=${LDAP_BASE_DN:-false}
@@ -69,7 +60,6 @@ if [ ! -f ".env" ]; then
       LDAP_PASS=${LDAP_PASS:-false}
       LDAP_USER_FILTER=${LDAP_USER_FILTER:-false}
       LDAP_VERSION=${LDAP_VERSION:-false}
-
       # Mail settings
       MAIL_DRIVER=${MAIL_DRIVER:-smtp}
       MAIL_HOST=${MAIL_HOST:-localhost}
@@ -87,7 +77,7 @@ fi
 
 echoerr "wait-for-db: waiting for ${DB_HOST_NAME}:${DB_PORT}"
 
-timeout 15 bash <<EOT
+timeout 30 bash <<EOT
 while ! (echo > /dev/tcp/${DB_HOST_NAME}/${DB_PORT}) >/dev/null 2>&1;
     do sleep 1;
 done;
@@ -96,7 +86,7 @@ RESULT=$?
 
 if [ $RESULT -eq 0 ]; then
   # sleep another second for so that we don't get a "the database system is starting up" error
-  sleep 1
+  sleep 10
   echoerr "wait-for-db: done"
 else
   echoerr "wait-for-db: timeout out after 15 seconds waiting for ${DB_HOST_NAME}:${DB_PORT}"
